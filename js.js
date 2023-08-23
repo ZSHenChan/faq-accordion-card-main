acb844da4ca700a57e5ad528ca79f316506081f0
Zhen <zishenchan@gmail.com>
2023-08-23 19:15:04 +0800

    completed mobile UI
    to-do:
    desktop UI

---
+++ js.js
@@ -0,0 +1,69 @@
+function answer1(){
+    const dropbox = document.getElementById("answer-1");
+    const ques = document.getElementById("question-1");
+    if (dropbox.style.display=="none"){
+        dropbox.style.display="flex";
+        ques.style.color = "hsl(238, 29%, 16%)";
+        ques.style.fontWeight = "700";
+    }
+    else{
+        dropbox.style.display="none";
+        ques.style.fontWeight="400";
+    }
+}
+
+function answer2(){
+    const dropbox = document.getElementById("answer-2");
+    const ques = document.getElementById("question-2");
+    if (dropbox.style.display=="none"){
+        dropbox.style.display="flex";
+        ques.style.color = "hsl(238, 29%, 16%)";
+        ques.style.fontWeight = "700";
+    }
+    else{
+        dropbox.style.display="none";
+        ques.style.fontWeight="400";
+    }
+}
+
+function answer3(){
+    const dropbox = document.getElementById("answer-3");
+    const ques = document.getElementById("question-3");
+    if (dropbox.style.display=="none"){
+        dropbox.style.display="flex";
+        ques.style.color = "hsl(238, 29%, 16%)";
+        ques.style.fontWeight = "700";
+    }
+    else{
+        dropbox.style.display="none";
+        ques.style.fontWeight="400";
+    }
+}
+
+function answer4(){
+    const dropbox = document.getElementById("answer-4");
+    const ques = document.getElementById("question-4");
+    if (dropbox.style.display=="none"){
+        dropbox.style.display="flex";
+        ques.style.color = "hsl(238, 29%, 16%)";
+        ques.style.fontWeight = "700";
+    }
+    else{
+        dropbox.style.display="none";
+        ques.style.fontWeight="400";
+    }
+}
+
+function answer5(){
+    const dropbox = document.getElementById("answer-5");
+    const ques = document.getElementById("question-5");
+    if (dropbox.style.display=="none"){
+        dropbox.style.display="flex";
+        ques.style.color = "hsl(238, 29%, 16%)";
+        ques.style.fontWeight = "700";
+    }
+    else{
+        dropbox.style.display="none";
+        ques.style.fontWeight="400";
+    }
+}
